f41e5c4ecc8691782331ae01dbd890edf15e1fdf
ain88 <ahyungss@hotmail.com>
2022-09-28 16:26:10 -0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -28,31 +28,31 @@ const social = [
 const project = [
     {
       "id": 1,
-      "imgUrl": "img/project-film-festival.png",
-      "aHref": "https://ain88.github.io/film-festival/",
-      "name": "Boston Film Festival",
-      "desc": "Html, Css, Web design",
-      "desc2": "Built film festival website using bootstrap/html/css."
+      "imgUrl": "img/project-sddocs.png",
+      "aHref": "https://docs.sdcloudpos.com",
+      "name": "User Manual Web App",
+      "desc": "Asp.net Core, C#, Html, Css",
+      "desc2": "Built user manual web application and admin page using asp.net core."
     },
     {
       "id": 2,
-      "imgUrl": "img/project-luxury.png",
-      "aHref": "https://dev-luxury-chalets-2019.pantheonsite.io/home/",
-      "name": "Luxury Charlets",
-      "desc": "Wordpress, Web development",
-      "desc2": "Real estate website using wordpress, modified and applied a theme and plugin accordigly."
+      "imgUrl": "img/project-ampm-ppc.png",
+      "aHref": "https://www.ampmservice.com/ppc-pos",
+      "name": "Company Landing Page",
+      "desc": "Asp.net Core, Javascript, Html, Css",
+      "desc2": "Created company landing page."
     },
     {
       "id": 3,
-      "imgUrl": "img/project-ice-cream.png",
+      "imgUrl": "img/project-ampmwebsite.png",
       "aHref": "https://icecream-museum.herokuapp.com/",
       "name": "Icecream Museum",
       "desc": "Bootstrap, Web page design",
-      "desc2": "Single page html bootstrap theme displaying information of ice cream museum."
+      "desc2": "Re-designed."
     },
     {
       "id": 4,
-      "imgUrl": "img/project-react-portfolio.png",
+      "imgUrl": "img/project-ampm-localexpress.png",
       "aHref": "https://ain88.github.io/portfolio/",
       "name": "Portfolio",
       "desc": "React, Bootstrap",
@@ -60,7 +60,7 @@ const project = [
     },
     {
       "id": 5,
-      "imgUrl": "img/project-expressfood.png",
+      "imgUrl": "img/project-supremebanners.png",
       "aHref": "./img/mysql-diagram.png",
       "name": "Express Food Order",
       "desc": "Mysql",
@@ -68,6 +68,78 @@ const project = [
     },
     {
       "id": 6,
+      "imgUrl": "img/project-vector3.png",
+      "aHref": "https://move-groove-rails.herokuapp.com/",
+      "name": "Move & Groove",
+      "desc": "Ruby on Rails",
+      "desc2": "Activity tracker built with Ruby on rails"
+    },
+    {
+      "id": 7,
+      "imgUrl": "img/project-cannabis-coupon.png",
+      "aHref": "https://ain88.github.io/portfolio/",
+      "name": "Portfolio",
+      "desc": "React, Bootstrap",
+      "desc2": "Portfolio webpage using react,bootstrap, made it re-usable."
+    },
+    {
+      "id": 8,
+      "imgUrl": "img/project-estwestmeds.png",
+      "aHref": "./img/mysql-diagram.png",
+      "name": "Express Food Order",
+      "desc": "Mysql",
+      "desc2": "Food order system database with class diagram, mysql"
+    },
+    {
+      "id": 9,
+      "imgUrl": "img/project-fedroc-holdings.png",
+      "aHref": "https://move-groove-rails.herokuapp.com/",
+      "name": "Move & Groove",
+      "desc": "Ruby on Rails",
+      "desc2": "Activity tracker built with Ruby on rails"
+    },
+    {
+      "id": 10,
+      "imgUrl": "img/project-film-festival.png",
+      "aHref": "https://ain88.github.io/film-festival/",
+      "name": "Boston Film Festival",
+      "desc": "Html, Css, Web design",
+      "desc2": "Built film festival website using bootstrap/html/css."
+    },
+    {
+      "id": 11,
+      "imgUrl": "img/project-luxury.png",
+      "aHref": "https://dev-luxury-chalets-2019.pantheonsite.io/home/",
+      "name": "Luxury Charlets",
+      "desc": "Wordpress, Web development",
+      "desc2": "Real estate website using wordpress, modified and applied a theme and plugin accordigly."
+    },
+    {
+      "id": 12,
+      "imgUrl": "img/project-ice-cream.png",
+      "aHref": "https://icecream-museum.herokuapp.com/",
+      "name": "Icecream Museum",
+      "desc": "Bootstrap, Web page design",
+      "desc2": "Single page html bootstrap theme displaying information of ice cream museum."
+    },
+    {
+      "id": 13,
+      "imgUrl": "img/project-react-portfolio.png",
+      "aHref": "https://ain88.github.io/portfolio/",
+      "name": "Portfolio",
+      "desc": "React, Bootstrap",
+      "desc2": "Portfolio webpage using react,bootstrap, made it re-usable."
+    },
+    {
+      "id": 14,
+      "imgUrl": "img/project-expressfood.png",
+      "aHref": "./img/mysql-diagram.png",
+      "name": "Express Food Order",
+      "desc": "Mysql",
+      "desc2": "Food order system database with class diagram, mysql"
+    },
+    {
+      "id": 15,
       "imgUrl": "img/project-move-groove.png",
       "aHref": "https://move-groove-rails.herokuapp.com/",
       "name": "Move & Groove",
